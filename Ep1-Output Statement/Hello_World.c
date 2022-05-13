57d289dd1676e456bc9d159538a018dd0924f40b
Edric-Ato <105485282+Edric-Ato@users.noreply.github.com>
2022-05-13 16:39:02 +0800

    Add files via upload

---
+++ Ep1-Output Statement/Hello_World.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //standard i/o -> standard input and output
+
+int main(void) { //start node
+	printf("Hi!\n"); //print format
+	printf("My name is Edric");
+	
+	return 0; //end node
+}
